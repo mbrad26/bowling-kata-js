1544e4337ade79f77ede946efc41ae5dab0cfa84
mbrad26 <mbrad26@gmail.com>
2020-12-07 16:36:37 +0000

    calculates when all roles are 1

--- spec/game.test.js
+++ spec/game.test.js
@@ -1,17 +1,31 @@
 const Game = require('../lib/game');
 
 describe('Game', () => {
-  const game = new Game();
+  let game;
+
+  beforeEach(() => {
+    game = new Game();
+  });
 
   it('is defined', () => {
     expect(game).toBeDefined();
   });
 
-  it('calculates a getter game', () => {
+  it('calculates a gutter game', () => {
     for(let i = 0; i < 20; i++) {
       game.roll(0);
     };
 
     expect(game.score()).toEqual(0);
+  });
+
+  it('calculates when all roles are 1', () => {
+    for(let i = 0; i < 20; i++) {
+      game.roll(1);
+    }
+    
+    console.log('GAME: ', game);
+    
+    expect(game.score()).toEqual(20);
   });
 });
