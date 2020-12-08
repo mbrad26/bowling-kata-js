33906ba51e2e6ff3968409edea482e0ac90a9530
mbrad26 <mbrad26@gmail.com>
2020-12-08 09:30:44 +0000

    refactor test suits

--- spec/game.test.js
+++ spec/game.test.js
@@ -7,24 +7,24 @@ describe('Game', () => {
     game = new Game();
   });
 
+  const rollMany = (n, pins) => {
+    for(let i = 0; i < n; i++) {
+      game.roll(pins);
+    };
+  };
+
   it('is defined', () => {
     expect(game).toBeDefined();
   });
 
   it('calculates a gutter game', () => {
-    for(let i = 0; i < 20; i++) {
-      game.roll(0);
-    };
+    rollMany(20, 0);
 
     expect(game.score()).toEqual(0);
   });
 
   it('calculates when all roles are 1', () => {
-    for(let i = 0; i < 20; i++) {
-      game.roll(1);
-    }
-    
-    console.log('GAME: ', game);
+    rollMany(20, 1);
     
     expect(game.score()).toEqual(20);
   });
